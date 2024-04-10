b81ef1736135bb617f3d9021286324492e995946
Altery <113447896+Alterys@users.noreply.github.com>
2024-04-11 01:19:36 +0300

    update ForecastDayModel and delete GetForecastDayUseCase.kt

--- app/src/main/java/com/example/weather/presentation/screens/weather/model/ForecastDayModel.kt
+++ app/src/main/java/com/example/weather/presentation/screens/weather/model/ForecastDayModel.kt
@@ -4,11 +4,19 @@ import com.example.weather.data.remote.response.ForecastDay
 
 data class ForecastDayModel(
     val date: String,
-    val temp: Int,
-    val icon: String
+    val minTemp: Double,
+    val maxTemp: Double,
+    val icon: String,
+    val textCondition: String
 )
 fun ForecastDay.toForecastDayModel(): ForecastDayModel {
-        return ForecastDayModel(this.date, this.day.avgTemp.toInt(), this.day.condition.icon)
+        return ForecastDayModel(
+            this.date,
+            this.day.minTemp,
+            this.day.maxTemp,
+            this.day.condition.icon,
+            this.day.condition.textForecastCondition
+        )
 }
 
 
