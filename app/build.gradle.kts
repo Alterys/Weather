f953156e71f20490a92944b06ee1b67461f332ed
Altery <113447896+Alterys@users.noreply.github.com>
2024-04-14 02:56:35 +0300

    add dependencies navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.coil)
     implementation(libs.converter.gson)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
