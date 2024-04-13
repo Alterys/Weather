68a42408b1a3072e6c2eb6ea54300f066a7c2982
Altery <113447896+Alterys@users.noreply.github.com>
2024-04-14 02:57:52 +0300

    add navigation

--- app/src/main/java/com/example/weather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weather/presentation/MainActivity.kt
@@ -8,6 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.weather.presentation.screens.city.manager.CityManagerScreen
+import com.example.weather.presentation.screens.city.search.CitySearchScreen
+import com.example.weather.presentation.screens.city.search.CitySearchViewModel
 import com.example.weather.presentation.screens.weather.WeatherScreen
 import com.example.weather.presentation.screens.weather.WeatherViewModel
 import com.example.weather.presentation.ui.theme.WeatherTheme
@@ -22,12 +30,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                 ) {
                     val weatherViewModel: WeatherViewModel by viewModels()
-                    val state = weatherViewModel.screenState.collectAsState().value
-                    WeatherScreen(
-                        screenState = state,
-                        getWeather = weatherViewModel::getWeather,
-                        searchCity = weatherViewModel::searchCity
-                    )
+                    val citySearchViewModel: CitySearchViewModel by viewModels()
+                    val stateWeather = weatherViewModel.screenState.collectAsState().value
+                    val stateCitySearch = citySearchViewModel.screenState.collectAsState().value
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "cityManager"
+                    ) {
+                        composable(
+                            route = "cityManager"
+                        ) {
+                            CityManagerScreen(
+                                navController = navController
+                            )
+                        }
+                        composable(
+                            route = "citySearch",
+                        ) {
+                            CitySearchScreen(
+                                onNavigateToWeather = { navController.navigate("citySearch/$it") },
+                                screenState = stateCitySearch,
+                                searchCity = citySearchViewModel::searchCity
+                            )
+                        }
+                        composable(
+                            route = "weather/{city}",
+                            arguments = listOf(navArgument("city") { type = NavType.StringType})
+                        ) {
+                            val city = it.arguments?.getString("city") ?: ""
+                            WeatherScreen(
+                                city = city,
+                                navController = navController,
+                                screenState = stateWeather,
+                                getWeather = weatherViewModel::getWeather
+                            )
+                        }
+                    }
                 }
             }
         }
