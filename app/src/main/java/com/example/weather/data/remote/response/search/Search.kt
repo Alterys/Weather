a664f84e676e679faae73821d255e941c70bc509
Altery <113447896+Alterys@users.noreply.github.com>
2024-04-14 13:00:01 +0300

    add city search columns

--- app/src/main/java/com/example/weather/data/remote/response/search/Search.kt
+++ app/src/main/java/com/example/weather/data/remote/response/search/Search.kt
@@ -3,5 +3,7 @@ package com.example.weather.data.remote.response.search
 import com.google.gson.annotations.SerializedName
 
 data class Search(
-    @SerializedName("name") val nameSearch: String
+    @SerializedName("name") val nameSearch: String,
+    @SerializedName("region") val regionSearch: String,
+    @SerializedName("country") val countrySearch: String
 )
